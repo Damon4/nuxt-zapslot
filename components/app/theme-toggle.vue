e4c35f5182996bdf968e509357da9ff2f948bf4c
Dmitry <dark.knight.9111@gmail.com>
2025-07-19 14:24:22 +0300

    add layout, home

---
+++ components/app/theme-toggle.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const colorMode = useColorMode()
+const isDark = computed({
+  get: () => colorMode.value === 'dark',
+  set: (value) => {
+    colorMode.preference = value ? 'dark' : 'light'
+  },
+})
+</script>
+
+<template>
+  <label class="swap swap-rotate">
+    <input v-model="isDark" type="checkbox" >
+    <Icon class="swap-on" name="tabler:moon" size="24" />
+    <Icon class="swap-off" name="tabler:sun" size="24" />
+  </label>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hero bg-base-200 container mx-auto min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">ZapSlot</h1>
+        <p class="py-6">
+          Welcome! Here you can easily book an appointment for the service you
+          need. Simply choose your desired service, select a convenient time,
+          and fill out the form. We will contact you to confirm your booking.
+          Fast, easy, and no unnecessary calls!
+        </p>
+        <button class="btn btn-accent">
+          Sign in with GitHub
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
