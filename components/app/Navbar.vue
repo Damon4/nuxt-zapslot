b5dfffbab1070e47b92093dc8f4173ff1afd5938
Dmitry <dark.knight.9111@gmail.com>
2025-07-19 20:24:48 +0300

    refactor(NavBar): enhance user authentication display with session management

--- components/app/Navbar.vue
+++ components/app/Navbar.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { NuxtLink } from '#components'
 import AuthButton from '~/components/app/AuthButton.vue'
+import { authClient } from '~/lib/auth-client'
+const session = authClient.useSession()
 </script>
 
 <template>
@@ -12,7 +14,25 @@ import AuthButton from '~/components/app/AuthButton.vue'
       </NuxtLink>
     </div>
     <div class="navbar-end">
-      <AuthButton />
+      <div v-if="session?.data" class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <img
+              alt="User avatar"
+              :src="session.data.user?.image ?? undefined"
+            >
+          </div>
+        </div>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        >
+          <li>
+            <button @click="authClient.signOut()">Logout</button>
+          </li>
+        </ul>
+      </div>
+      <AuthButton v-if="!session?.data" />
       <AppThemeToggle class="ml-4" />
     </div>
   </div>
