ebaf53346642ea044be8ea88ee4295abc255f2ff
Dmitry <dark.knight.9111@gmail.com>
2025-07-19 13:27:54 +0300

    add nuxt icon

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <button class="btn btn-secondary">Secondary</button>
+  <button class="btn btn-secondary">
+    Secondary
+    <Icon name="tabler:pencil" size="20" />
+  </button>
 </template>
