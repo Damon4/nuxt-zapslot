93253fae8252f6815f91099b5e03227251288022
Dmitry <dark.knight.9111@gmail.com>
2025-08-08 12:46:08 +0300

    chore: update nuxt version to 4.0.3 in package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/color-mode', '@pinia/nuxt'],
